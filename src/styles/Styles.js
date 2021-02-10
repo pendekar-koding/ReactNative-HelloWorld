698b13f5098c6365b2c8ea1d0ffe7129ecd6a640
pendekar-koding <pendekarkoding20@gmail.com>
2021-02-10 20:15:21 +0700

    update code

---
+++ src/styles/Styles.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  welcome: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default Styles;
